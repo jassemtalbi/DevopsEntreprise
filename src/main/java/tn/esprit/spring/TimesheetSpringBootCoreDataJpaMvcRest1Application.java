7877975f17ad3acb183857a5499466153ec4da9b
jassemtalbi <49127296+jassemtalbi@users.noreply.github.com>
2021-11-04 16:16:27 +0100

    'Stupid2'

--- src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
+++ src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class TimesheetSpringBootCoreDataJpaMvcRest1Application {
 
 	public static void main(String[] args) {
